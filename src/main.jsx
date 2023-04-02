ad88896f11f62c5e60ffaa569e8b3d05a1768cb5
ehmunna <emran-hussain.munna-tu-ilmenau.de>
2023-04-02 17:35:13 +0200

    new comment and update

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,37 @@ import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import ErrorPage from './errorPage';
 
+// 54-1 Module Introduction, simple react router setup
+
 // in this createBrowserRouter() method component bare minimum we need to property in an object
+
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <App></App>,
+//     errorElement: <ErrorPage></ErrorPage>,
+//   },
+
+//   //  create new routes
+//   {
+//     path: `/about`,
+//     element: <div>This is about</div>,
+//   },
+//   {
+//     path: '/contact',
+//     element: <div>This is contact</div>,
+//   },
+// ]);
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <RouterProvider router={router}></RouterProvider>
+//   </React.StrictMode>
+// );
+
+//
+
+// 54-2 (interesting) Nested route and outlet for shared content
 
 const router = createBrowserRouter([
   {
