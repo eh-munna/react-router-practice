2e1e717a6b4611e4ce40fc9bd22be6506a4eecfd
ehmunna <emran-hussain.munna-tu-ilmenau.de>
2023-04-02 17:16:59 +0200

    set up router and error page simple intro

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,30 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ErrorPage from './errorPage';
+
+// in this createBrowserRouter() method component bare minimum we need to property in an object
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <div>Try with router</div>,
+    element: <App></App>,
+    errorElement: <ErrorPage></ErrorPage>,
+  },
+
+  //  create new routes
+  {
+    path: `/about`,
+    element: <div>This is about</div>,
+  },
+  {
+    path: '/contact',
+    element: <div>This is contact</div>,
   },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>
 );
